0a614c79127aea1da5c388e46e923c8d31749f7d
JosherLo <h1910091@nushigh.edu.sg>
2021-08-27 17:53:22 +0800

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module Hackathon2021 {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    opens app.Controller;
+}
